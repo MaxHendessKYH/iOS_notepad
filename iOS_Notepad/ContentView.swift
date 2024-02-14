933bd811bd3a9b973ccd101dd0395ea382d50340
maxmac <nekohigan@gmail.com>
2024-02-14 10:45:21 +0100

    Initial Commit

---
+++ iOS_Notepad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Notepad
+//
+//  Created by Max.Hendess on 2024-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_Notepad/iOS_NotepadApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_NotepadApp.swift
+//  iOS_Notepad
+//
+//  Created by Max.Hendess on 2024-02-14.
+//
+
+import SwiftUI
+
+@main
+struct iOS_NotepadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
